62a9f492baff2252724a8eb0f8ee99c72dbe48de
KILLER42 <40739278+KILLER42@users.noreply.github.com>
2024-01-04 17:38:13 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,24 @@
-// Спрашиваем у пользователя, что он хочет сделать
-var operation = prompt("Выберите действие (add, sub, mult, div):");
+'use strict';
 
-// Спрашиваем у пользователя первое число
-var firstNumber = parseFloat(prompt("Введите первое число:"));
+const action = prompt('Что вы хотите сделать? Выберите: add, sub, mult, div');
+const firstNumber = Number(prompt('Введите первое число'));
+const secondNumber = Number(prompt('Введите второе число'));
+const additionResult = Number(firstNumber + secondNumber);
+const subtractionResult = Number(firstNumber - secondNumber);
+const multiplicationResult = Number(firstNumber * secondNumber);
+const divisionResult = Number(firstNumber / secondNumber);
 
-// Проверяем, является ли введенное значение числом
-if (isNaN(firstNumber)) {
-    alert("Пожалуйста, введите корректное число.");
-} else {
-    // Спрашиваем у пользователя второе число
-    var secondNumber = parseFloat(prompt("Введите второе число:"));
-
-    // Проверяем, является ли введенное значение числом
-    if (isNaN(secondNumber)) {
-        alert("Пожалуйста, введите корректное число.");
-    } else {
-        // Выполняем выбранную операцию и выводим результат
-        var result;
-        switch (operation) {
-            case 'add':
-                result = firstNumber + secondNumber;
-                break;
-            case 'sub':
-                result = firstNumber - secondNumber;
-                break;
-            case 'mult':
-                result = firstNumber * secondNumber;
-                break;
-            case 'div':
+switch (action) {
+    case 'add':
+        alert(firstNumber + ' + ' + secondNumber + ' = ' + additionResult);
+        break;
+    case 'sub':
+        alert(firstNumber + ' - ' + secondNumber + ' = ' + subtractionResult);
+        break;
+    case 'mult':
+        alert(firstNumber + ' * ' + secondNumber + ' = ' + multiplicationResult);
+        break;
+	case 'div':
                 // Проверяем деление на 0
                 if (secondNumber === 0) {
                     alert("На 0 делить нельзя.");
@@ -35,14 +26,7 @@ if (isNaN(firstNumber)) {
                     result = firstNumber / secondNumber;
                 }
                 break;
-            default:
-                alert("Неверная операция. Выберите add, sub, mult или div.");
-                break;
-        }
-
-        // Выводим результат операции
-        if (result !== undefined) {
-            alert(firstNumber + " " + operation + " " + secondNumber + " = " + result);
-        }
-    }
+    default:
+        alert('Выберите правильное действие!');
+        break;
 }
